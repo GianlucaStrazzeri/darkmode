1798a95936ceb26fcc6808865ea39ca4843683da
GianlucaStrazzeri <githubandwebprojects@gmail.com>
2023-12-06 18:26:33 +0100

    Añadiendo toggle button

---
+++ js/script.js
@@ -0,0 +1,27 @@
+//Capturo la clase .button es un botón elongado en html
+const button=document.querySelector(".button")
+//Imprimo en consola para confirmar de estar en el sitio correcto
+console.log(button);
+//Capturo la clase .button-circle es un circulo en html
+const toggle=document.querySelector(".button-circle")
+//Imprimo en consola para confirmar de estar en el sitio correcto
+console.log(toggle);
+
+//Creo un evento, al hacer click cambiará caolor con la función cambiarColor que definiré a continuación
+toggle.addEventListener("click",cambiarColor);
+
+function cambiarColor(){
+    //Al hacer click en el botón verde aparecerá en la consola, funciona. Eso implicará 
+    //que está escuchando el evento al hacer click enciama del botón redondo 
+    console.log("Funciona")
+    //Añado una clase y remuevo la existencia en stylesheet de la clase button-circle pondré una clase 
+    //con el nombre cambiarBotón de background-color rosa y con las mismas dimensiones del botón de antes
+    //puedo hacerlo con toggle.classList.add("cambiarBotón") y toggle.classListe.remove("button-circle")
+    //o con la función toggle de Js
+toggle.classList.toggle("cambiarBotón");
+toggle.classList.toggle("button-circle");
+
+
+};
+    
+
